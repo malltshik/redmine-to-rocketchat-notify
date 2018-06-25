3c7175fe5eede783d23b326ec37311b5b1819330
Артем Гаврилов <gavrilov@tecforce.ru>
2018-06-25 11:21:11 +0300

    Небольшие изменения

--- src/main/kotlin/ru/tecforce/worktime/Clients.kt
+++ src/main/kotlin/ru/tecforce/worktime/Clients.kt
@@ -4,7 +4,6 @@ import org.apache.http.conn.ssl.NoopHostnameVerifier
 import org.apache.http.conn.ssl.SSLConnectionSocketFactory
 import org.apache.http.conn.ssl.TrustStrategy
 import org.apache.http.impl.client.HttpClients
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.web.client.RestTemplateBuilder
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory
 import org.springframework.stereotype.Component
@@ -13,20 +12,14 @@ import java.text.SimpleDateFormat
 import java.util.*
 
 @Component
-class RedmineClient(restTemplateBuilder: RestTemplateBuilder) {
-
-    @Value("\${redmine.address}")
-    val address: String? = null
-
-    @Value("\${redmine.token}")
-    val token: String? = null
+class RedmineClient(val props: RedmineProperties, restTemplateBuilder: RestTemplateBuilder) {
 
     val restTemplate: RestTemplate = restTemplateBuilder.requestFactory {
         val requestFactory = HttpComponentsClientHttpRequestFactory()
         try {
             val acceptingTrustStrategy = TrustStrategy {
-                chain: Array<out java.security.cert.X509Certificate>?,
-                authType: String -> true
+                _: Array<out java.security.cert.X509Certificate>?,
+                _: String -> true
             }
             val sslContext = org.apache.http.ssl.SSLContexts.custom()
                     .loadTrustMaterial(null, acceptingTrustStrategy)
@@ -41,18 +34,23 @@ class RedmineClient(restTemplateBuilder: RestTemplateBuilder) {
 
     // TODO fetch all users with bunch by bunch with offset parameter
     fun fetchUsers(): Array<User> {
-        val body = this.restTemplate.getForEntity("${address}/users.json?key=${token}", UsersMessage::class.java).body
-        return body?.users ?: emptyArray<User>()
+        val body = this.restTemplate.getForEntity(url("users"), UsersMessage::class.java).body
+        return body?.users ?: emptyArray()
     }
 
-    // TODO worklogs clould be more then ${limit} default 100
+    // TODO worklogs could be more then ${limit} default 100
     fun fetchWorklogs(id: Long, date: Date): Array<Worklog> {
         val spendOn = SimpleDateFormat("YYYY-mm-dd").format(date);
-        val body = this.restTemplate.getForEntity(
-                "${address}/time_entries.json?id=${id}&spent_on=${spendOn}&limit=100&key=${token}",
+        val body = this.restTemplate.getForEntity(url("time_entries",
+                mapOf("spent_on" to spendOn, "id" to "$id", "limit" to "100")),
                 WorklogMessage::class.java).body
-        return body?.worklogs ?: emptyArray<Worklog>()
+        return body?.worklogs ?: emptyArray()
+    }
+
+    private fun url(path: String, params: Map<String, String> = emptyMap()): String {
+        var url = "${props.address}/$path.json?key=${props.token}&"
+        params.forEach { k, v -> url += "$k=$v&" }
+        return url
     }
 
 }
-
--- src/main/kotlin/ru/tecforce/worktime/WorktimeApplication.kt
+++ src/main/kotlin/ru/tecforce/worktime/WorktimeApplication.kt
@@ -1,11 +1,9 @@
 package ru.tecforce.worktime
 
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
 import org.springframework.scheduling.annotation.EnableScheduling
-import org.springframework.stereotype.Service
 
 @SpringBootApplication
 @EnableConfigurationProperties(RedmineProperties::class)
